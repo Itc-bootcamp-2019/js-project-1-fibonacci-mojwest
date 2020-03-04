577762e0cbf2d45bdb71d337279c432bd5cd5bb7
Jonathan Westrich <mojwest@gmail.com>
2020-03-04 12:43:54 +0200

    milestone 3 complete

--- Westrich-Fibonacci/script.js
+++ Westrich-Fibonacci/script.js
@@ -1,2 +1,20 @@
-document.getElementById("x").innerText = "7";
-document.getElementById("y").innerText = "13";
+function fibonacci(x) {
+  let firstPrev = 0;
+  let secondPrev = 1;
+  let fiboResult;
+
+  for (let i = 1; i < x; i++) {
+    fiboResult = secondPrev + firstPrev;
+    firstPrev = secondPrev;
+    secondPrev = fiboResult;
+  }
+
+  return fiboResult;
+}
+
+let x = 13;
+let fibo = fibonacci(x);
+console.log(fibo);
+
+document.getElementById("x").innerText = x;
+document.getElementById("y").innerText = fibo;
