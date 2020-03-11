db9ba95b161a4957f4d7ba7593842f689a107772
Jonathan Westrich <mojwest@gmail.com>
2020-03-11 13:28:27 +0200

    here's my milestone 6

--- Westrich-Fibonacci/script.js
+++ Westrich-Fibonacci/script.js
@@ -1,27 +1,68 @@
-function fibonacci(x) {
-  let firstPrev = 0;
-  let secondPrev = 1;
-  let fiboResult;
-
-  for (let i = 1; i < x; i++) {
-    fiboResult = secondPrev + firstPrev;
-    firstPrev = secondPrev;
-    secondPrev = fiboResult;
-  }
-
-  return fiboResult;
+function showSpinner() {
+  document.getElementById("loader").style.visibility = "visible";
+}
+function hideSpinner() {
+  document.getElementById("loader").style.visibility = "hidden";
+}
+function hideMol() {
+  document.getElementById("mol").style.visibility = "hidden";
+}
+function showMol() {
+  document.getElementById("mol").style.visibility = "visible";
+}
+function hideTooBig() {
+  document.getElementById("toobig").style.visibility = "hidden";
+}
+function showTooBig() {
+  document.getElementById("toobig").style.visibility = "visible";
+}
+function hideResult() {
+  document.getElementById("result").style.visibility = "hidden";
+  // document.getElementById("result").style.display = "none";
+}
+function showResult() {
+  document.getElementById("result").style.visibility = "visible";
 }
 
 function fibonacciServer() {
+  showSpinner();
   let userInput = document.getElementById("fibo-input").value;
   console.log(userInput);
-  fetch(`http://localhost:5050/fibonacci/${userInput}`)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.result);
-      document.getElementById("result").innerText = data.result;
-    })
-    .catch(err => console.error(err));
+  if (userInput > 50) {
+    console.log("too big");
+    document.getElementById("toobig").innerText =
+      "Sorry, that number is too big.";
+    hideMol();
+    hideResult();
+    showTooBig();
+    hideSpinner();
+  } else {
+    let url = `http://localhost:5050/fibonacci/${userInput}`;
+    fetch(url)
+      .then(response => {
+        if (response.status == 400) {
+          showMol();
+          hideResult();
+          hideTooBig();
+          throw response;
+        }
+        hideMol();
+        showResult();
+        hideTooBig();
+        return response.json();
+      })
+      .then(data => {
+        hideSpinner();
+        console.log(data.result);
+        document.getElementById("result").innerText = data.result;
+      })
+      .catch(error => error.text())
+      .then(meaningOfLife => {
+        hideSpinner();
+        document.getElementById("mol").innerText = meaningOfLife;
+        console.log(meaningOfLife);
+      });
+  }
 }
 
 document
